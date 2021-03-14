bd6de839861c7e2a9ab30a8635f0404ff4179a32
Arpit Gupta <imarg_3@zoho.com>
2021-03-15 01:10:36 +0530

    Flask based microservice

---
+++ flask-app/main/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://gibeeuvk:uP6VPF2eXt9aulsp9WjALuW7twR9BEcJ@puffin.rmq2.cloudamqp.com/gibeeuvk')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
